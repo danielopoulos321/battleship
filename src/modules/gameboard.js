141d9e336a0ce57e65564b797ebe2c75a98f3b50
Daniel Anestopoulos <dstops97@gmail.com>
2023-10-31 13:37:48 -0400

    Create gameboard array

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,1 +1,6 @@
-export default class Gameboard {}
+export default class Gameboard {
+  board = new Array(10).fill().map(() => new Array(10).fill(0));
+}
+
+//  Check if ship placement is higher than 0 and less than 10 in both directions
+//  Board spots can be objects containing name of ship and hit status
