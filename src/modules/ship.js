9ffb4676f545524f88f779987ba0ec9e756987d0
Daniel Anestopoulos <dstops97@gmail.com>
2023-10-30 22:49:40 -0400

    Add initial Ship class and basic functions

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,1 +1,15 @@
-export default class Ship {}
+export default class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.hits += 1;
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
