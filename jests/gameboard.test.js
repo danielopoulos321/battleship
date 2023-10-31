2193661025dc1254d7b14d323d9b23c8f54e46f8
Daniel Anestopoulos <dstops97@gmail.com>
2023-10-31 13:37:15 -0400

    Test gameboard creation

--- jests/gameboard.test.js
+++ jests/gameboard.test.js
@@ -1,1 +1,18 @@
 import Gameboard from "../src/modules/gameboard";
+
+describe("Gameboard", () => {
+  let gameboard;
+
+  beforeEach(() => {
+    gameboard = new Gameboard();
+  });
+
+  test("board rows and columns are 10", () => {
+    expect(gameboard.board.length).toBe(10);
+    expect(gameboard.board[0].length).toBe(10);
+  });
+
+  test("places new ship", () => {});
+
+  test("does not place ship if coordinate is occupied", () => {});
+});
