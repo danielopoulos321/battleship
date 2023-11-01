dc1166fb72a3a643943e3857064f51ca02769a9f
Daniel Anestopoulos <dstops97@gmail.com>
2023-10-31 20:51:24 -0400

    Create and test valid placement function

--- jests/gameboard.test.js
+++ jests/gameboard.test.js
@@ -12,7 +12,40 @@ describe("Gameboard", () => {
     expect(gameboard.board[0].length).toBe(10);
   });
 
-  test("places new ship", () => {});
+  test("places new ship", () => {
+    gameboard.placeShip("destroyer", 4, [1, 1], true);
+    expect(gameboard.board[1][1].shipName).toBe("destroyer");
+  });
+
+  test("places new ship", () => {
+    gameboard.placeShip("destroyer", 4, [1, 1], false);
+    expect(gameboard.board[1][3].shipName).toBe("destroyer");
+  });
 
   test("does not place ship if coordinate is occupied", () => {});
+});
+
+describe("Gameboard Private Functions", () => {
+  let gameboard;
+
+  beforeEach(() => {
+    gameboard = new Gameboard();
+  });
+
+  test("checks valid placement", () => {
+    expect(gameboard.isValidPlacement(4, 1, 1, true)).toBe(true);
+  });
+
+  test("checks placement out of bounds", () => {
+    expect(gameboard.isValidPlacement(4, 7, 7, true)).toBe(false);
+  });
+
+  test("checks placement where ship already exists", () => {
+    gameboard.board[2][1].shipName = "destroyer";
+    expect(gameboard.isValidPlacement(4, 1, 1, true)).toBe(false);
+  });
+
+  test("checks placement where horizontal would be valid but not vertical", () => {
+    expect(gameboard.isValidPlacement(4, 1, 8, false)).toBe(false);
+  });
 });
--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,6 +1,37 @@
 export default class Gameboard {
-  board = new Array(10).fill().map(() => new Array(10).fill(0));
-}
+  board = new Array(10)
+    .fill()
+    .map(() => new Array(10).fill({ shipName: null, hit: false }));
 
-//  Check if ship placement is higher than 0 and less than 10 in both directions
-//  Board spots can be objects containing name of ship and hit status
+  placedShips = [];
+
+  receiveAttack(coords) {}
+  //  If location has name of ship, hit status is true and ship.hit()
+
+  placeShip(shipName, length, coords, horizontal) {}
+  //  If placement is valid
+  //  Iterate through coords + ship.length
+  //  Create new ship object
+  //  Change each spot object to contain ship name
+  //  Push ship object to placedShips
+
+  isValidPlacement(length, x, y, horizontal) {
+    if (horizontal) {
+      if (x + length > 9) return false;
+      for (let i = 0; i < length; i += 1) {
+        if (this.board[x + i][y].shipName !== null) {
+          return false;
+        }
+      }
+    } else {
+      if (y + length > 9) return false;
+      for (let i = 0; i < length; i += 1) {
+        if (this.board[x][y + i].shipName !== null) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
