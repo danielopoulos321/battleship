1c2e71841b4574e6d1a59c4e3ec3ed72cb32b76a
Daniel Anestopoulos <dstops97@gmail.com>
2023-11-01 14:40:42 -0400

    Add basic player class + test files

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,1 +1,14 @@
-export default class Player {}
+import Gameboard from "./gameboard";
+
+export default class Player {
+  constructor() {
+    this.gameboard = new Gameboard();
+    this.turn = false;
+  }
+}
+
+//  Each player generates a gameboard
+//  When attacking it targets the other player's gameboard
+// Function to switch turns
+// Function for AI to attack random valid spot
+// Function for AI to place its ships randomly
