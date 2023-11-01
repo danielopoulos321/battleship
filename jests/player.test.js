fcf71852226eb7e241f9b142b1d0281e65669fec
Daniel Anestopoulos <dstops97@gmail.com>
2023-11-01 15:15:24 -0400

    Add turn switching and basic attack function

--- jests/player.test.js
+++ jests/player.test.js
@@ -1,1 +1,27 @@
-describe("Player", () => {});
+import Player from "../src/modules/player";
+
+describe("Player", () => {
+  let player1;
+  let player2;
+
+  beforeEach(() => {
+    player1 = new Player();
+    player2 = new Player();
+
+    player1.gameboard.placeShip("destroyer", 4, [1, 1], true);
+  });
+
+  test("switches turns", () => {
+    player1.takeTurn();
+    expect(player1.turn).toBe(true);
+    player1.endTurn(player2);
+    expect(player1.turn).toBe(false);
+    expect(player2.turn).toBe(true);
+  });
+
+  test("attacks other player", () => {
+    expect(player1.gameboard.board[1][1].hit).toBe(false);
+    player2.attack(player1, [1, 1]);
+    expect(player1.gameboard.board[1][1].hit).toBe(true);
+  });
+});
--- src/modules/player.js
+++ src/modules/player.js
@@ -5,10 +5,22 @@ export default class Player {
     this.gameboard = new Gameboard();
     this.turn = false;
   }
+
+  takeTurn() {
+    this.turn = true;
+  }
+
+  endTurn(player) {
+    this.turn = false;
+    player.takeTurn();
+  }
+
+  attack(player, coords) {
+    player.gameboard.receiveAttack(coords);
+    this.endTurn();
+  }
 }
 
-//  Each player generates a gameboard
 //  When attacking it targets the other player's gameboard
-// Function to switch turns
 // Function for AI to attack random valid spot
 // Function for AI to place its ships randomly
