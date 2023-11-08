fa41d5bac4ea4668c1d122d045aeade3b7b91ef8
Daniel Anestopoulos <dstops97@gmail.com>
2023-11-07 22:51:36 -0500

    Create named players in initialization

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,8 +1,8 @@
 import Player from "./player";
 
 export default function initGame() {
-  const player = new Player();
-  const computer = new Player();
+  const player = new Player("You");
+  const computer = new Player("Enemy");
   player.pcGenerateFleet();
   computer.pcGenerateFleet();
   player.takeTurn();
